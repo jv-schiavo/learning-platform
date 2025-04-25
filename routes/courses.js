c72c4c37debafa5e363ec0d426a24ce370ad02cb
joao victor schiavo <jvgs.schiavo@gmail.com>
2025-04-25 19:50:19 +0100

    Amend code to export the function properly

--- routes/courses.js
+++ routes/courses.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { showCourses } = require('../controllers/coursesController');
 
-router.get('courses/', showCourses);
+router.get('/courses', showCourses);
 
 module.exports = router;
 
